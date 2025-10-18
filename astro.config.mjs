785c4a5ccee44a3ba172b615e3c582f6cc7add89
Saya-such <work@such-web.com>
2025-10-18 12:15:11 +0900

    chore: 必要パッケージのインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+//開発モードか公開用(GitHub Pages)か判別
+const isGhPages = process.env.DEPLOY_ENV === "gh-pages";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: isGhPages ? "/such-portfolio/" : "",
+  build: {
+    assetsPrefix: "./",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `_astro/[hash].js`,
+          chunkFileNames: `_astro/[hash].js`,
+          assetFileNames: (assetInfo) => {
+            // 画像はimagesフォルダへ(それ以外は_astroフォルダへ)
+            if (
+              /\.(png|jpg|jpeg|gif|svg|webp|avif)$/.test(assetInfo.name ?? "")
+            ) {
+              return "images/[name].[hash][extname]";
+            }
+            return "_astro/[name].[hash][extname]";
+          },
+        },
+      },
+    },
+  },
+});
