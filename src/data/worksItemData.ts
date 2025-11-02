a29eae60580ad29f4158719c2c847377dba8b468
Saya-such <work@such-web.com>
2025-11-02 12:22:45 +0900

    feat: style home components and add images

---
+++ src/data/worksItemData.ts
@@ -0,0 +1,16 @@
+import type { WorksItemData } from "@/types/WorksItemData";
+
+const worksItemData: WorksItemData[] = [
+  {
+    title: "Astro Demo Page",
+    img: "./images/such-demo1-caputure.jpg",
+    link: "#",
+  },
+  {
+    title: "ポートフォリオ",
+    img: "./images/such-demo1-caputure.jpg",
+    link: "#",
+  },
+];
+
+export default worksItemData;
