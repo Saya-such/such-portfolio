5a4f20db83dfc8f7da1a5db9afcf03ed3925bb7d
Saya-such <work@such-web.com>
2025-11-13 11:16:33 +0900

    feat: implement common fade-in and scroll-trigger animations

---
+++ src/libs/animations/startFadeInOnScroll.ts
@@ -0,0 +1,25 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import type { FadeInProps } from "@/types/FadeInProps";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const startFadeInOnScroll = ({
+  target,
+  duration = 0.5,
+  options = {},
+}: FadeInProps) => {
+  return gsap.from(target, {
+    y: 3,
+    opacity: 0,
+    duration,
+    ease: "power1.in",
+    scrollTrigger: {
+      trigger: target,
+      start: "top 80%",
+    },
+    ...options,
+  });
+};
+
+export default startFadeInOnScroll;
---
+++ src/types/FadeInProps.ts
@@ -0,0 +1,7 @@
+type FadeInProps = {
+  target: HTMLElement;
+  duration?: number;
+  options?: GSAPTweenVars;
+};
+
+export type { FadeInProps };
